81012f7bcdb5e80638f5b56015adf9ee22ed2ef3
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-28 21:57:30 +0530

    Using redux toolkit createSlice and configureStore to create slice of related states in the store with different reducer functions

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,28 @@
 // const redux = require("redux"); //way to import in node.js application
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 const initialState = { counter: 0, showCounter: true };
-const counterReduxer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-  return state;
-};
 
-const store = createStore(counterReduxer);
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
