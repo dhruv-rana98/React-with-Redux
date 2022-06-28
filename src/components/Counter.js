5b28063758ffc8422c4c24757ebf51cce007b829
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-28 12:39:49 +0530

    Dispatching an action to redux store using useDispatch hook from react-redux to change the state based on type of action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,26 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const counter = useSelector((state) => state.counter); //redux state value fetched using a function
+  const dispatch = useDispatch(); //create a dispatch fucntion using useDispatch to dispacth action
+  const counter = useSelector((state) => state.counter); //redux state value fetched using a function to output value from the redux store
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
